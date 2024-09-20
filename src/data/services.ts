217f361707f70f84372f0fa8a126a2eeb76c01f7
Shin-sibainu <shincode0712@gmail.com>
2024-09-20 09:03:29 +0900

    change gif file path

--- src/data/services.ts
+++ src/data/services.ts
@@ -3,36 +3,36 @@ export const services = [
     title: "Speed",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, excepturi?",
-    url: "/src/assets/gif/rocket-unscreen.gif",
+    url: "/gif/rocket-unscreen.gif",
   },
   {
     title: "Save Money",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, excepturi?",
-    url: "/src/assets/gif/save-money-unscreen.gif",
+    url: "/gif/save-money-unscreen.gif",
   },
   {
     title: "Support",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, excepturi?",
-    url: "/src/assets/gif/tech-support-unscreen.gif",
+    url: "/gif/tech-support-unscreen.gif",
   },
   {
     title: "PC Design",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, excepturi?",
-    url: "/src/assets/gif/laptop-unscreen.gif",
+    url: "/gif/laptop-unscreen.gif",
   },
   {
     title: "Security",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, excepturi?",
-    url: "/src/assets/gif/shield-unscreen.gif",
+    url: "/gif/shield-unscreen.gif",
   },
   {
     title: "Settings",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, excepturi?",
-    url: "/src/assets/gif/settings-unscreen.gif",
+    url: "/gif/settings-unscreen.gif",
   },
 ];
